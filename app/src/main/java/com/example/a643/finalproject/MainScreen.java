24d1fcfe62807cb2a9d51c4a3031c662f3f2784c
MichelleBeni <35224688+MichelleBeni@users.noreply.github.com>
2018-01-08 15:46:16 +0200

    Update MainScreen.java

--- app/src/main/java/com/example/a643/finalproject/MainScreen.java
+++ app/src/main/java/com/example/a643/finalproject/MainScreen.java
@@ -8,12 +8,27 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainScreen extends AppCompatActivity {
-    TextView SignOut;
+  TextView SignOut;
+    Intent intent;
+    Context context;
     FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        SignOut= (TextView)findViewById(R.id.SignOut);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser= firebaseAuth.getCurrentUser();
+        SignOut.setOnClickListener(this);
+        context= this;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        firebaseAuth.signOut();
+        intent = new Intent(context,Enter_Activity.class);
+        startActivity(intent);
 
     }
 }
