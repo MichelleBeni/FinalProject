e6aac326443d7f0a4c7f599885fa5189d70a5fc6
MichelleBeni <35224688+MichelleBeni@users.noreply.github.com>
2018-01-08 15:39:17 +0200

    //version1(LoginANDLogOutFinish)

--- app/src/main/java/com/example/a643/finalproject/MainScreen.java
+++ app/src/main/java/com/example/a643/finalproject/MainScreen.java
@@ -1,19 +1,37 @@
 package com.example.a643.finalproject;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainScreen extends AppCompatActivity {
+public class MainScreen extends AppCompatActivity implements View.OnClickListener {
     TextView SignOut;
+    Intent intent;
+    Context context;
     FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        SignOut= (TextView)findViewById(R.id.SignOut);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser= firebaseAuth.getCurrentUser();
+        SignOut.setOnClickListener(this);
+        context= this;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        firebaseAuth.signOut();
+        intent = new Intent(context,Enter_Activity.class);
+        startActivity(intent);
 
     }
 }
