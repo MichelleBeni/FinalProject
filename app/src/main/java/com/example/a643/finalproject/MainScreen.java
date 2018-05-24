f1150104f64ad93f3fa7f8c842c9ec4c30390f06
MichelleBeni <35224688+MichelleBeni@users.noreply.github.com>
2018-05-24 15:30:29 +0300

    //VERSION7MENU

--- app/src/main/java/com/example/a643/finalproject/MainScreen.java
+++ app/src/main/java/com/example/a643/finalproject/MainScreen.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -27,13 +31,10 @@ import java.util.ArrayList;
 import dmax.dialog.SpotsDialog;
 
 public class MainScreen extends AppCompatActivity implements View.OnClickListener {
-    TextView SignOut;
+
     Intent intent;
     Context context;
     FirebaseAuth firebaseAuth;
-    Button search;
-    Button addAd;
-    ImageView myProfile;
     ListView lv;
     ArrayList<Ad> ads;
     private android.app.AlertDialog loading;
@@ -57,6 +58,8 @@ public class MainScreen extends AppCompatActivity implements View.OnClickListene
 
         firebaseStorage = FirebaseStorage.getInstance();
         storageReference=firebaseStorage.getReference();
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
 
 
@@ -66,17 +69,13 @@ public class MainScreen extends AppCompatActivity implements View.OnClickListene
 
 
 
-        myProfile = (ImageView) findViewById(R.id.myProfile);
-        myProfile.setOnClickListener(this);
-        SignOut= (TextView)findViewById(R.id.SignOut);
+
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser= firebaseAuth.getCurrentUser();
-        SignOut.setOnClickListener(this);
-        search= (Button) findViewById(R.id.search_ad);
-        search.setOnClickListener(this);
+
         context= this;
-        addAd=(Button)findViewById(R.id.AddAd);
-        addAd.setOnClickListener(this);
+      /*  addAd=(Button)findViewById(R.id.AddAd);
+        addAd.setOnClickListener(this);*/
     }
 
     public void retriveData()
@@ -114,28 +113,55 @@ public class MainScreen extends AppCompatActivity implements View.OnClickListene
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.searchAd:
+
+                intent = new Intent(context,SearchAd.class);
+                startActivity(intent);
+                return true;
+
+            case  R.id.signOut:
+                firebaseAuth.signOut();
+                intent = new Intent(context, Enter_Activity.class);
+                startActivity(intent);
+                return true;
+
+            case  R.id.addNewAd:
+                intent = new Intent(context,AddAd.class);
+                startActivity(intent);
+                return  true;
+
+            case  R.id.editAd:
+                intent = new Intent(context,EditAds.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    @Override
     public void onClick(View v) {
 
-       if(v==SignOut) {
-           firebaseAuth.signOut();
-           intent = new Intent(context, Enter_Activity.class);
-           startActivity(intent);
-       }
-       if(v==search)
-       {
-           intent = new Intent(context,SearchAd.class);
-           startActivity(intent);
-       }
-       if(v==addAd)
-       {
-           intent = new Intent(context,AddAd.class);
-           startActivity(intent);
-       }
-       if(v==myProfile)
-       {
-           intent = new Intent(context,MyProfile.class);
-           startActivity(intent);
-       }
+
+
+
 
     }
 }
